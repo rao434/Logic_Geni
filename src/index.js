bc53eb60b0cc1200929a812c53c5fa0a54876712
Rao Qurban <raoqurban56@gmail.com>
2023-05-27 17:24:45 +0000

    Icons added.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import '@fortawesome/fontawesome-free/css/all.css';
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" />;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
